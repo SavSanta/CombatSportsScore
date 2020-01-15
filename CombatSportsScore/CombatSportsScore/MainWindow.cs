aaa171296de3470d06c430e8bcc14bf017c99764
Ru Uba <remethyst@yahoo.com>
2020-01-15 07:44:29 -0500

    Bump version in about dialog

--- CombatSportsScore/CombatSportsScore/MainWindow.cs
+++ CombatSportsScore/CombatSportsScore/MainWindow.cs
@@ -77,7 +77,7 @@ public partial class MainWindow : Gtk.Window
         abdg.WrapLicense = true;
         abdg.Authors = new string[] { "SavSanta <@SavSantaX>" };
         abdg.ProgramName = "Combat Sports ScoreCard Keeper";
-        abdg.Version = "0.95";
+        abdg.Version = "0.98";
         abdg.Website = "https://github.com/SavSanta/CombatSportsScore";
         abdg.Comments = "Combat Sports (Boxing/MMA/KickBoxing) Scorecard Keeper written in C# using GTK+";
         abdg.License = "Permission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\nTHE SOFTWARE.";
